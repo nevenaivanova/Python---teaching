94f0f6346fbc85c7c43afb149232cfb1a5e3e641
nevenaivanova <65981481+nevenaivanova@users.noreply.github.com>
2021-03-20 13:20:02 +0200

    Add files via upload

---
+++ Exercise_06/Exercise_06_solutions.py
@@ -0,0 +1,48 @@
+#Example1
+string= str(input())
+print(string)
+string1=str(input())
+print(string.title())
+print(string.lower())
+print(string.upper())
+
+
+#Task1 and Task2
+str0=str(input())
+str1=str(input())
+str2="apple"
+str3="Orange"
+
+print(srt0.title())
+print(str1.upper())
+print(str2.lower())
+print(str2+str3)
+
+#Task3
+print("Languages:\nHTML\nCSS\nPython\nJavaScript)
+
+#Task4
+name=str(input())
+print("Hello" + name + ", would you like to learn some Python today?")
+      
+#Task 5
+first_name=str(input())
+second_name=str(input())
+full_name=first_name+ " " + second_name
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
+
+#Task 6
+print("Albert Einstein once said,\"A person who never made a mistake never tried anything new./"")
+
+#Task 7
+x="Hello world"
+print('H' in x)
+print('hello' not in x)
+print('Hel' in x)
+print("Hello" in x)
+
+#Task 8
+
+      
