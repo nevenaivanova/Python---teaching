2321a8e593255b4a8f00b74242fd443f44ce75f3
nevenaivanova <65981481+nevenaivanova@users.noreply.github.com>
2021-04-01 14:04:08 +0300

    Add files via upload

---
+++ Exercise_07/Solutions_07.py
@@ -0,0 +1,38 @@
+"""
+#Task 1
+pizza_list=["margarita", "peperoni, "capricosa"]
+for i in list:
+    print(i)
+
+
+
+#Task 2
+
+fruit_list=["cherry","berry","orange"]
+for x in fruit_list:
+    (print(fruit_list)) #add a comment
+    
+for x in range(len(list)):
+    print (list[x]) #add a comment
+
+#Task 3
+drink_list=["Cola", "water" ,"juice"]
+for x in drink_list:
+    print("I like", x)
+    #print("I like %s" % x) - друг вариант за принтираме
+
+"""
+#Task 4
+list1= [12, 15, 32, 42, 55, 75, 80, 122, 132, 150, 180, 200]
+
+for x in list1:
+    if(x % 5 == 0):
+        if(x>150): break
+        else: print(x)
+
+#for x in list1: 
+        if(x>150):
+            break
+        if (x %5 == 0):
+            print(x)
+
